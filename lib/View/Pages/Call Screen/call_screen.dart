221f7df51e68ab133712601540273ef747d97ee2
Samar Singh <singhsamar2002@gmail.com>
2024-04-29 00:54:09 +0530

    in app call added

--- lib/View/Pages/Call Screen/call_screen.dart
+++ lib/View/Pages/Call Screen/call_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
+
 import 'package:contacts_service/contacts_service.dart';
 import 'package:fluttemp/Utils/constants/app_constants.dart';
 import 'package:fluttemp/Utils/provider/app_providers.dart';
 import 'package:fluttemp/View/shared/app_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class CallScreen extends ConsumerWidget {
   const CallScreen({super.key});
@@ -24,11 +27,21 @@ class CallScreen extends ConsumerWidget {
           : ListView.builder(
               itemCount: contacts.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  onTap: () {},
-                  title: AppText.defaultText(
-                      contacts[index].displayName.toString(),
-                      context: context),
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    tileColor: AppColorsTheme.dark().bgInput.withAlpha(150),
+                    onTap: () {
+                      launchUrlString(
+                          "tel://${contacts[index].phones!.first.value}");
+                    },
+                    title: AppText.defaultText(
+                        size: 20,
+                        contacts[index].displayName.toString(),
+                        context: context),
+                  ),
                 );
               },
             ),
